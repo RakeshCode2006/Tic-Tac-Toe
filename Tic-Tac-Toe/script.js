7d04c4cddd966a3104c9a9c2afc70f3d64d2d6ab
RakeshCode2006 <rkshdinda@gmail.com>
2025-06-16 02:29:14 +0530

    Update script.js

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,13 +1,49 @@
 let boxes=document.querySelectorAll(".box");
 let box=document.querySelector("button");
 let rsb=document.querySelector(".rsb");
-let game=document.querySelector(".game");
-let container=document.querySelector(".container");
+let ngb=document.querySelector(".ng");
+let msgcont=document.querySelector(".hide");
+let msg=document.querySelector(".msg");
 let turn0=true;
+let cont=document.querySelector(".container");
 
 
 const winpat=[[0,1,2,],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]];
 
+
+const showwinner=(val)=>{
+    msg.innerText=`Congretulation!..Winner is ${val}`;
+    msgcont.classList.add("nonhide");
+    for(let box of boxes){
+        box.disable=true;
+    }
+    cont.classList.add("hide");
+    rsb.classList.add("hide");  
+}
+const winner = () =>{
+    for(let pattern of winpat){
+        console.log(pattern[0],pattern[1],pattern[2]);
+        let val1=boxes[pattern[0]].innerText;
+        let val2 =boxes[pattern[1]].innerText;
+        let val3 =boxes[pattern[2]].innerText;
+        if(val1 !="" && val2 !="" && val3 !="")
+        { 
+            if(val1 === val2 && val2 === val3)
+            {
+                   console.log(`Winner is ${val1}`);
+                   showwinner(val1);
+            }
+        }
+    }
+} 
+const newgame=()=>{
+    turn0=true;
+     for(let box of boxes){
+        box.innerText=" ";
+        box.disabled=false;
+    }
+    msgcont.classList.add("hide");
+}
 
 boxes.forEach((box,idx)=>{
     box.addEventListener("click",()=>
@@ -24,28 +60,10 @@ boxes.forEach((box,idx)=>{
         }
         console.log("clicked ",idx,"th box");
         box.disabled=true;
+        winner();
     });
-    winner();
 
 });
 
-const winner = () =>{
-    for(let pattern of winpat){
-        console.log(pattern[0],pattern[1],pattern[2]);
-        console.log(box[pattern[0]].innerText,
-            box[pattern[1]].innerText,
-            box[pattern[2]].innerText);
-        // let val1=boxes[pattern[0]].innerText;
-        // let val2 =boxes[pattern[1]].innerText;
-        // let val3 =boxes[pattern[2]].innerText;
-        // if(val1 !="" && val2 !="" && val3 !="")
-        // { 
-        //     if(val1 === val2 && val2 === val3)
-        //     {
-        //            console.log("winner");
-        //         //  return 0;
-        //     }
-         
-        // }
-    }
-} 
+rsb.addEventListener("click",newgame);
+ngb.addEventListener("click",newgame);
